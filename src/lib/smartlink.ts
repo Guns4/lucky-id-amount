6b2ac2d9d53e4919cf81d7452132c07742cc424d
Guns4 <muchamad.guntur04@gmail.com>
2025-12-17 11:53:36 +0700

    update

--- src/lib/smartlink.ts
+++ src/lib/smartlink.ts
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-export const SMARTLINK_URL =
-  "https://www.effectivegatecpm.com/q05d93hq?key=4284a1a2623ec09d7db4450d18e475e0";
+export const SMARTLINK_URL = "https://www.effectivegatecpm.com/q05d93hq?key=4284a1a2623ec09d7db4450d18e475e0";
 
 export function openSmartlink() {
   window.open(SMARTLINK_URL, "_blank", "noopener,noreferrer");
 }
-=======
-export const SMARTLINK_URL =
-  "https://www.effectivegatecpm.com/q05d93hq?key=4284a1a2623ec09d7db4450d18e475e0";
-
-export function openSmartlink() {
-  window.open(SMARTLINK_URL, "_blank", "noopener,noreferrer");
-}
->>>>>>> 97c73a6 (update)
